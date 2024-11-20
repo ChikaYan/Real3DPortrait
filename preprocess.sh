4237a10cb7f8b85217665490663339fd0bb6b3cc
WalterWu <walterwuyan@gmail.com>
2024-11-20 15:27:39 +0000

    Update

--- preprocess.sh
+++ preprocess.sh
@@ -1,2 +1,8 @@
-python extract_segment_imgs.py --vid_dir /home/tw554/Real3DPortrait/data/TH1KH_512_jingyang3/video \
-    --num_workers 10 --force_single_process
+# python extract_segment_imgs.py --vid_dir /home/tw554/Real3DPortrait/data/TH1KH_512_jingyang3/video \
+#     --num_workers 10 --force_single_process
+
+
+
+# python extract_lm2d.py --vid_dir /home/tw554/Real3DPortrait/data/TH1KH_512_jingyang3/video 
+
+python fit_3dmm_landmark.py --vid_dir /home/tw554/Real3DPortrait/data/TH1KH_512_jingyang3/video --ds_name TH1KH_512
